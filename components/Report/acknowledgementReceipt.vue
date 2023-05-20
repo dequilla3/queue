5a222e10ea582ad9b6dfa2e2a72df50406d22015
Kim Ariel Dequilla <deqkim4@gmail.com>
2023-05-20 10:23:02 +0800

    added acknowledgement receipt

---
+++ components/Report/acknowledgementReceipt.vue
@@ -0,0 +1,257 @@
+<template>
+  <div class="report">
+    <div class="head-section">
+      <div class="logo pt-4">
+        <img class="mr-2" src="../../static/logo.png" alt="" />
+        <p>
+          <b class="f13"> PHILIPPINE CROP</b> <br />
+          <b class="f13"> INSURANCE CORPORATION</b> <br />
+          <span class="sub-text">REGION XI</span>
+        </p>
+      </div>
+      <div class="title">
+        <h6 class="pt-3 pb-3 bt2">
+          <b>ACKNOWLEDGEMENT RECEIPT</b>
+        </h6>
+        <h4 class="bt2 pb-2">No. 0001</h4>
+        <p class="">Date: {{ new Date().toLocaleDateString() }}</p>
+      </div>
+    </div>
+
+    <div class="body_section">
+      <div class="b2 pt-1 pb-1 pl-1 payor">
+        PAYOR:
+        <h4 class="ml-2">KIM ARIEL DEQUILLA</h4>
+      </div>
+      <div>
+        <table class="">
+          <tr>
+            <th>PRODUCT LINE</th>
+            <th>AMOUNT</th>
+          </tr>
+          <tr v-for="item in productLine" :key="item.index">
+            <td :class="`pl-2 ${item.class}`">{{ item.prodName }}</td>
+            <td :class="`pl-2 ${item.class}`">{{ item.price }}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+
+    <div class="footer_section">
+      <div class="b2 pl-1"><b>AMOUNT IN WORDS</b></div>
+      <div class="b2 pt-2 pb-1">
+        <div class="text-center">
+          <b class="f16">{{ getNumToWord }}</b>
+        </div>
+      </div>
+
+      <div class="payment_type">
+        <div class="payment_type_group">
+          <div class="payment_type_select">
+            <div class="payment_type_select_box mr-2 selected_payment" />
+            <b>CASH</b>
+          </div>
+          <div class="payment_type_select">
+            <div class="payment_type_select_box mr-2" />
+            <b>CHECK</b>
+          </div>
+        </div>
+
+        <div class="payment_type_table">
+          <div class="payment_type_tr">
+            <div class="bt2 pb-1"><b>Drawee Bank</b></div>
+            <div class="payment_type_td"></div>
+          </div>
+          <div class="payment_type_tr">
+            <div class="bt2 pb-1"><b>Number</b></div>
+            <div class="payment_type_td"></div>
+          </div>
+          <div class="payment_type_tr">
+            <div class="bt2 pb-1"><b>Date</b></div>
+            <div class="payment_type_td"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="signature">
+        <div class="co">
+          <u><b>Mitchell Barcelona</b></u>
+          <div>Collecting Officer</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { convertNuWithCents } from "../../util/convertNumbertoWords";
+export default {
+  data() {
+    return {
+      productLine: [
+        {
+          prodName: "RICE",
+          price: 2000,
+          class: "",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    fillInBlank() {
+      let prodLen = this.productLine.length;
+
+      for (let i = 0; i < 8 - prodLen; i++) {
+        console.log(i);
+        if (i == 8) {
+          this.productLine.push({
+            prodName: "TOTAL",
+            price: "2000",
+            class: "font-weight-bold",
+          });
+        } else {
+          this.productLine.push({
+            prodName: "-",
+            price: "-",
+            class: "",
+          });
+        }
+      }
+    },
+  },
+
+  computed: {
+    getNumToWord() {
+      return convertNuWithCents(2000).toUpperCase();
+    },
+  },
+
+  created() {
+    this.fillInBlank();
+  },
+};
+</script>
+<style scoped>
+* {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.payor {
+  display: flex;
+}
+.h100 {
+  height: 100%;
+}
+.report {
+  margin: 0.5in;
+  border: 2px solid #000000;
+  width: 5.5in;
+  height: 7.5in;
+}
+
+.head-section {
+  display: flex;
+}
+
+.logo {
+  border: 2px solid #000000;
+  display: flex;
+  padding: 5px;
+  width: 50%;
+}
+
+.title {
+  border: 2px solid #000000;
+  width: 50%;
+  text-align: center;
+}
+
+.f13 {
+  font-size: 13px;
+}
+
+.f16 {
+  font-size: 16px;
+}
+
+.bt2 {
+  border-bottom: 2px solid #000000;
+}
+
+img {
+  width: 50px;
+  height: 50px;
+
+  /* padding: 20px; */
+}
+.b2 {
+  border: 2px solid #000000;
+}
+
+.sub-text {
+  font-size: 10px;
+}
+
+table {
+  border: 2px solid #000000;
+  width: 100%;
+}
+th {
+  border: 2px solid #000000;
+  padding: 10px 5px;
+}
+td {
+  border: 2px solid #000000;
+  /* color: transparent; */
+}
+
+.payment_type {
+  border: 2px solid #000000;
+  display: flex;
+}
+
+.payment_type_select {
+  display: flex;
+}
+
+.payment_type_select_box {
+  border: 2px solid #000000;
+  height: 18px;
+  width: 25px;
+}
+.payment_type_group {
+  border: 2px solid #000000;
+  padding: 10px;
+  width: 200px;
+}
+
+.payment_type_table {
+  display: flex;
+  width: 100%;
+}
+.payment_type_tr {
+  border: 2px solid #000000;
+  width: 100%;
+  text-align: center;
+}
+
+.payment_type_table_data {
+  height: 100%;
+}
+
+.payment_type_th {
+  border: 2px solid #000000;
+}
+
+.selected_payment {
+  border: 9px solid #000000;
+}
+
+.signature {
+  text-align: center;
+}
+
+.co {
+  padding-top: 35px;
+}
+</style>
--- util/convertNumbertoWords.js
+++ util/convertNumbertoWords.js
@@ -1,29 +1,95 @@
-
 function doConvert(n, custom_join_character) {
   if (n == undefined) {
-    return '';
+    return "";
   }
 
-  const numLen = n.toString().split('').length;
+  const numLen = n.toString().split("").length;
 
   let string = n.toString(),
-    units, tens, scales, start, end, chunks, chunksLen, chunk, ints, i, word, words;
+    units,
+    tens,
+    scales,
+    start,
+    end,
+    chunks,
+    chunksLen,
+    chunk,
+    ints,
+    i,
+    word,
+    words;
 
-  let and = custom_join_character || 'and';
+  let and = custom_join_character || "and";
 
   /* Is number zero? */
   if (parseInt(string) === 0) {
-    return 'zero';
+    return "zero";
   }
 
   /* Array of units as words */
-  units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  units = [
+    "",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+  ];
 
   /* Array of tens as words */
-  tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  tens = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ];
 
   /* Array of scales as words */
-  scales = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quatttuor-decillion', 'quindecillion', 'sexdecillion', 'septen-decillion', 'octodecillion', 'novemdecillion', 'vigintillion', 'centillion'];
+  scales = [
+    "",
+    "thousand",
+    "million",
+    "billion",
+    "trillion",
+    "quadrillion",
+    "quintillion",
+    "sextillion",
+    "septillion",
+    "octillion",
+    "nonillion",
+    "decillion",
+    "undecillion",
+    "duodecillion",
+    "tredecillion",
+    "quatttuor-decillion",
+    "quindecillion",
+    "sexdecillion",
+    "septen-decillion",
+    "octodecillion",
+    "novemdecillion",
+    "vigintillion",
+    "centillion",
+  ];
 
   /* Split user arguemnt into 3 digit chunks from right to left */
   start = string.length;
@@ -36,19 +102,17 @@ function doConvert(n, custom_join_character) {
   /* Check if function has enough scale words to be able to stringify the user argument */
   chunksLen = chunks.length;
   if (chunksLen > scales.length) {
-    return '';
+    return "";
   }
 
   /* Stringify each integer in each chunk */
   words = [];
   for (i = 0; i < chunksLen; i++) {
-
     chunk = parseInt(chunks[i]);
 
     if (chunk) {
-
       /* Split chunk into array of individual integers */
-      ints = chunks[i].split('').reverse().map(parseFloat);
+      ints = chunks[i].split("").reverse().map(parseFloat);
 
       /* If tens integer is 1, i.e. 10, then add 10 to units integer */
       if (ints[1] === 1) {
@@ -72,29 +136,24 @@ function doConvert(n, custom_join_character) {
 
       /* Add 'and' string after units or tens integer if: */
       if (ints[0] || ints[1]) {
-
         /* Chunk has a hundreds integer or chunk is the first of multiple chunks */
-        if (ints[2] || !i && chunksLen && numLen != 2) {
+        if (ints[2] || (!i && chunksLen && numLen != 2)) {
           words.push(and);
         }
-
       }
 
       /* Add hundreds word if array item exists */
       if ((word = units[ints[2]])) {
-        words.push(word + ' hundred');
+        words.push(word + " hundred");
       }
-
     }
-
   }
 
-  return words.reverse().join(' ');
-
+  return words.reverse().join(" ");
 }
 
 export function convertNuWithCents(num) {
-  const nums = num.toString().split('.');
-  const cents = nums[1] ? ' and ' + doConvert(nums[1]) + ' cent' : '';
-  return doConvert(nums[0]) + cents;
+  const nums = num.toString().split(".");
+  const cents = nums[1] ? " and " + doConvert(nums[1]) + " cent/s" : "";
+  return `${doConvert(nums[0])} PESOS${cents} ONLY`;
 }
